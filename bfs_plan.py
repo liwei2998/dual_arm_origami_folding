618673bba513eedca2a93ee0bdea9d28cd22b492
liwei2998 <55369454+liwei2998@users.noreply.github.com>
2021-01-18 17:23:36 +0800

    Add files via upload

--- bfs_plan.py
+++ bfs_plan.py
@@ -4,56 +4,59 @@ Input consists of a simple graph of { node: [list of neighbors] } plus a source 
 """
 
 from collections import deque
+import origami_state_generation as osg
 
-def bfs(graph, src, tgt):
+facets1 = {"1":[[[1,1],[0,0]],[[0,0],[-1,1]]],
+          "2":[[[1,1],[0,0]],[[0,0],[1,-1]]],
+          "3":[[[0,0],[1,-1]],[[0,0],[-1,-1]]],
+          "4":[[[0,0],[-1,-1]],[[0,0],[-1,1]]]}
+
+polygen1 = {"1":[[1,1],[0,0],[-1,1]],
+            "2":[[1,1],[1,-1],[0,0]],
+            "3":[[1,-1],[0,0],[-1,-1]],
+            "4":[[0,0],[-1,-1],[-1,1]]}
+stack1 = [["1","2","3","4"]]
+state1 = {"stack":stack1,"polygen":polygen1,"facet_crease":facets1}
+state_dict = {"state1":state1}
+state_graph = {"state1":[]}
+
+def bfs(state_graph, src, tgt_stack):
     """Return the shortest path from the source (src) to the target (tgt) in the graph"""
 
-    if src in graph is False:
+    if src in state_graph is False:
         raise AttributeError("The source '%s' is not in the graph" % src)
-    if tgt in graph is False:
-        raise AttributeError("The target '%s' is not in the graph" % tgt)
+
 
     parents = {src: None}
     queue = deque([src])
+    k = 2
+    # #dynamic generate variable's names, using locals()
+    # names = locals()
     while queue:
         node = queue.popleft()
         print "node",node
-        for neighbor in graph[node]:
+        state_node = state_dict[node]
+        # generate children states for this node
+        children_states = osg.generateNextLayerStates(state_node)
+        for i in range(len(children_states)):
+            # names["state"+str(k)] = children_states[i]
+            #store each children states
+            state_dict["state"+str(k)] = children_states[i]
+            #add children states to state_graph
+            state_graph.setdefault(node,[]).append("state"+str(k))
+            k += 1
+        for neighbor in state_graph[node]:
             if neighbor not in parents:
                 parents[neighbor] = node
                 queue.append(neighbor)
-                if node == tgt:
+                if state_node["stack"] == tgt_stack:
                     break
-
-    path = [tgt]
-    while parents[tgt] is not None:
-        path.insert(0, parents[tgt])
-        tgt = parents[tgt]
+    path = [node]
+    while parents[node] is not None:
+        path.insert(0, parents[node])
+        node = parents[node]
 
     return path
 
-
-"""
-Test using the graph from:
-https://commons.wikimedia.org/wiki/File:Graph_with_Chordless_and_Chorded_Cycles.svg
-"""
-
-test = {
-    "a": ["b", "f"],
-    "b": ["a", "c", "g"],
-    "c": ["b", "d", "g", "l"],
-    "d": ["c", "e", "k"],
-    "e": ["d", "f"],
-    "f": ["a", "e"],
-    "g": ["b", "c", "h", "l"],
-    "h": ["g", "i"],
-    "i": ["h", "j", "k"],
-    "j": ["i", "k"],
-    "k": ["d", "i", "j", "l"],
-    "l": ["c", "g", "k"],
-}
-
-assert bfs(test, "a", "e") == ['a', 'f', 'e']
-assert bfs(test, "a", "i") == ['a', 'b', 'g', 'h', 'i']
-assert bfs(test, "a", "l") == ['a', 'b', 'c', 'l']
-assert bfs(test, "b", "k") == ['b', 'c', 'd', 'k']
+path = bfs(state_graph,"state1",[['3'],['4'],['1'],['2']])
+print "path",path
--- origami_state_generation.py
+++ origami_state_generation.py
@@ -11,7 +11,7 @@ polygen1 = {"1":[[1,1],[0,0],[-1,1]],
             "2":[[1,1],[1,-1],[0,0]],
             "3":[[1,-1],[0,0],[-1,-1]],
             "4":[[0,0],[-1,-1],[-1,1]]}
-state1 = [["1","2","3","4"]]
+stack1 = [["1","2","3","4"]]
 
 def ifLineColinear(line1,line2):
     '''
@@ -41,11 +41,11 @@ def CombineLinearLines(line1,line2):
     new_line = new_line.tolist()
     return new_line
 
-def findAllCreases(state,facets):
+def findAllCreases(stack,facets):
     creases = []
-    for i in range(len(state)):
-        for j in range(len(state[i])):
-            facet = state[i][j]
+    for i in range(len(stack)):
+        for j in range(len(stack[i])):
+            facet = stack[i][j]
             creases.append(facets[facet][0])
             creases.append(facets[facet][1])
 
@@ -67,24 +67,24 @@ def findMininalSetCrease(crease):
                 min_crease.append(line1)
     return min_crease
 
-crease = findAllCreases(state1,facets1)
+crease = findAllCreases(stack1,facets1)
 print "crease",crease
 min_crease = findMininalSetCrease(crease)
 print "min_crease",min_crease
 #how to set(min_crease)
 
-def findFeasibleCrease(crease,state,polygen):
+def findFeasibleCrease(crease,stack,polygen):
     feasible_crease=[]
-    for i in range(len(state)):
+    for i in range(len(stack)):
         for k in range(len(crease)):
             point1 = crease[k][0]
             point2 = crease[k][1]
-            for j in range(len(state[i])):
-                facet = state[i][j]
+            for j in range(len(stack[i])):
+                facet = stack[i][j]
                 poly = polygen[facet]
                 if is_inPoly(poly,point1)==1 or is_inPoly(poly,point2)==1:
                     break
-                elif j == len(state[i])-1:
+                elif j == len(stack[i])-1:
                     feasible_crease.append(crease[k])
     return feasible_crease
 
@@ -126,15 +126,15 @@ def lineToFunction(line):
         c = 1
         return a,b,c
 
-def divideStack(crease,state,polygon,sign):
+def divideStack(crease,stack,polygon,sign):
     base = []
     flap = []
     a,b,c = lineToFunction(crease)
-    for i in range(len(state)):
+    for i in range(len(stack)):
         base_tmp = []
         flap_tmp = []
-        for j in range(len(state[i])):
-            facet = state[i][j]
+        for j in range(len(stack[i])):
+            facet = stack[i][j]
             poly = polygon[facet]
             for k in range(len(poly)):
                 product = a*poly[k][0]+b*poly[k][1]+c
@@ -155,9 +155,9 @@ def divideStack(crease,state,polygon,sign):
         base.append(base_tmp)
         flap.append(flap_tmp)
     return base,flap
-feasible_crease = findFeasibleCrease(min_crease,state1,polygen1)
+feasible_crease = findFeasibleCrease(min_crease,stack1,polygen1)
 print "feasible_crease",feasible_crease
-base,flap = divideStack(feasible_crease[0],state1,polygen1,"+")
+base,flap = divideStack(feasible_crease[0],stack1,polygen1,"+")
 print "base_stack",base
 print "flap_stack",flap
 
@@ -177,3 +177,116 @@ def reverseStack(base,flap):
 
 new_stack = reverseStack(base,flap)
 print "new_stack",new_stack
+
+def reversePoint(crease,point):
+    a,b,c = lineToFunction(crease)
+    x = point[0]
+    y = point[1]
+    reversed_point = []
+    if a == 0:
+        x1 = x
+        y1 = -2*c/b - y
+    elif b == 0:
+        y1 = y
+        x1 = -2*c/a - x
+    elif a !=0 and b!= 0:
+        x1 = -1*(2*a*b*y + (a*a-b*b)*x + 2*a*c) / (a*a + b*b)
+        y1 = -1*((b*b-a*a)*y + 2*a*b*x + 2*b*c) / (a*a + b*b)
+    reversed_point.append(x1)
+    reversed_point.append(y1)
+    return reversed_point
+
+def reverseLine(crease,line):
+    point1 = line[0]
+    point2 = line[1]
+    reversed_line = []
+    new_point1 = reversePoint(crease,point1)
+    new_point2 = reversePoint(crease,point2)
+    reversed_line.append(new_point1)
+    reversed_line.append(new_point2)
+    return reversed_line
+
+def reverseCrease(flap,crease,facet_crease):
+    new_facet_crease = facet_crease
+    # remain creases in base stack
+    # reverse creases in flap stack
+    for i in range(len(flap)):
+        for j in range(len(flap[i])):
+            facet = flap[i][j]
+            creases = facet_crease[facet]
+            new_creases = []
+            for k in range(len(creases)):
+                new_creases.append(reverseLine(crease,creases[k]))
+            new_facet_crease[facet] = new_creases
+    return new_facet_crease
+
+def reversePolygen(flap,crease,polygon):
+    new_polygen = polygon
+    # remain polygens in base stack
+    # reverse polygens in flap stack
+    for i in range(len(flap)):
+        for j in range(len(flap[i])):
+            facet = flap[i][j]
+            poly = polygon[facet]
+            new_poly = []
+            for k in range(len(poly)):
+                new_poly.append(reversePoint(crease,poly[k]))
+            new_polygen[facet] = new_poly
+    return new_polygen
+
+reversed_polygen = reversePolygen(flap,feasible_crease[0],polygen1)
+print "reversed polygen",reversed_polygen
+reversed_creases = reverseCrease(flap,feasible_crease[0],facets1)
+print "reversed crease",reversed_creases
+
+def generateNextStateInformation(stack,polygon,facet_crease,crease,sign):
+    '''
+    Given a feasible crease fold, previous stack and polygen and facet_crease information,
+    return new stack and polygen and facet_crease information.
+    '''
+    #search for base and flap according to this crease
+    base,flap = divideStack(feasible_crease[0],stack1,polygen1,sign)
+
+    #generate new stack
+    reversed_stack = reverseStack(base,flap)
+
+    #generate new polygen
+    reversed_polygen = reversePolygen(flap,feasible_crease[0],polygen1)
+
+    #generate new facet creases
+    reversed_creases = reverseCrease(flap,feasible_crease[0],facets1)
+
+    return reversed_stack,reversed_polygen,reversed_creases
+
+state1 = {"stack":stack1,"polygen":polygen1,"facet_crease":facets1}
+
+def generateNextLayerStates(state):
+    '''
+    input parent node state information, return next layer's children states
+    '''
+    new_states = []
+    #find minimal set of lines taht contain all creases
+    crease = findAllCreases(state["stack"],state["facet_crease"])
+    min_crease = findMininalSetCrease(crease)
+    #how to set(min_crease)
+    #find all feasible creases
+    feasible_crease = findFeasibleCrease(min_crease,state["stack"],state["polygen"])
+    print "feasible crease",feasible_crease
+    #generate new states for each feasible crease
+    for i in range(len(feasible_crease)):
+        state_tmp = {}
+        new_stack,new_polygen,new_creases = generateNextStateInformation(state["stack"],state["polygen"],
+                                                                         state["facet_crease"],feasible_crease[i],
+                                                                         "+")
+        state_tmp["stack"] = new_stack
+        state_tmp["polygen"] = new_polygen
+        state_tmp["facet_crease"] = new_creases
+        new_states.append(state_tmp)
+    return new_states
+
+new_states = generateNextLayerStates(state1)
+print "new state",new_states
+print "new_state1",new_states[0]
+names = locals()
+names["state" + str(2)] = new_states[0]
+print "state2",state2
